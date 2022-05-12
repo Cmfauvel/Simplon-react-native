37b1eea86a6e866b6bba401c6ee8350062bb34c9
Arnaud <arnaud@revolv.ai>
2022-05-12 16:35:19 +0200

    create Store / App.ts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { createStore } from "redux";
+import indexReducer from './src/store/reducers/index.reducer';
+
+const store = createStore(indexReducer)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
